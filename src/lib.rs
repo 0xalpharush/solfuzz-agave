b29a4024bed6fe9b0e3ac821245b3e8119d980a0
Kunal Bhargava <kbhargava@jumptrading.com>
2024-12-13 19:16:39 +0000

    disable_account_loader_special_case

--- src/lib.rs
+++ src/lib.rs
@@ -262,6 +262,7 @@ static SUPPORTED_FEATURES: &[u64] = feature_list![
     partitioned_epoch_rewards_superfeature,
     get_sysvar_syscall_enabled,
     migrate_feature_gate_program_to_core_bpf,
+    disable_account_loader_special_case,
 ];
 
 // If the `CORE_BPF_PROGRAM_ID` variable is set, declares the default compute
